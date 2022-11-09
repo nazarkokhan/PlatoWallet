6d360ee446be74c5f7cd6ba02effd5d066937662
nazarkokhan <nazarkokhansomejob@gmail.com>
2022-11-09 12:39:18 +0200

    add logs before startup

--- Platipus.Wallet.Api/Program.cs
+++ Platipus.Wallet.Api/Program.cs
@@ -16,97 +16,122 @@ using Platipus.Wallet.Api.StartupSettings.ServicesRegistrations;
 using Platipus.Wallet.Domain.Entities.Enums;
 using Platipus.Wallet.Infrastructure.Persistence;
 using Serilog;
-
-var builder = WebApplication.CreateBuilder(args);
-builder.Host.UseSerilog(
-    (context, configuration) =>
-    {
-        configuration.EnableSelfLog(context)
-            .ReadFrom.Configuration(context.Configuration);
-    });
-
-var builderConfiguration = builder.Configuration;
-var services = builder.Services;
-
-const string gamesApiUrl = "https://test.platipusgaming.com/"; //TODO to config
-services
-    .AddTransient<ExceptionHandlerMiddleware>()
-    .AddControllers(
-        options =>
-        {
-            options.Filters.Add<SaveRequestActionFilterAttribute>(1);
-
-            options.Filters.Add<ActionResultFilterAttribute>(1);
-            options.Filters.Add<LoggingResultFilterAttribute>(2);
-        })
-    .AddJsonOptions(
-        options =>
-        {
-            options.JsonSerializerOptions.NumberHandling = JsonNumberHandling.WriteAsString | JsonNumberHandling.AllowReadingFromString;
-            options.JsonSerializerOptions.PropertyNamingPolicy = new JsonSnakeCaseNamingPolicy();
-            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-            options.JsonSerializerOptions.Converters.Add(new JsonBoolAsNumberStringConverter());
-        })
-    .AddJsonOptions(nameof(CasinoProvider.Dafabet),
-        options =>
-        {
-            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-        })
-    .AddJsonOptions(nameof(CasinoProvider.Openbox),
-        options =>
-        {
-            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-            options.JsonSerializerOptions.Converters.Add(new JsonUnixDateTimeConverter());
-        })
-    .Services
-    .Configure<SupportedCurrenciesOptions>(builderConfiguration.GetSection(nameof(SupportedCurrenciesOptions)).Bind)
-    .Configure<SupportedCountriesOptions>(builderConfiguration.GetSection(nameof(SupportedCountriesOptions)).Bind)
-    .AddEndpointsApiExplorer()
-    .AddSwaggerGen()
-    .AddMediatR(Assembly.GetExecutingAssembly())
-    .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
-    .AddAllBehaviors()
-    .AddLocalization()
-    .AddLazyCache()
-    .AddDbContext<WalletDbContext>(
-        (provider, optionsBuilder) =>
-        {
-            optionsBuilder
-                .UseNpgsql(builderConfiguration.GetConnectionString(nameof(WalletDbContext)));
-
-            if (builder.Environment.IsDevelopment())
-            {
-                optionsBuilder.UseLoggerFactory(provider.GetRequiredService<ILoggerFactory>());
-                optionsBuilder.EnableSensitiveDataLogging();
-            }
-        })
-    .AddSingleton<IGamesApiClient, GamesApiClient>()
-    .AddTransient<RequestSignatureRelegatingHandler>()
-    .AddHttpClient<IGamesApiClient, GamesApiClient>(options => { options.BaseAddress = new Uri($"{gamesApiUrl}psw/"); })
-    .AddHttpMessageHandler<RequestSignatureRelegatingHandler>()
-    .Services
-    .AddSingleton<IDatabetGamesApiClient, DatabetGamesApiClient>()
-    .AddHttpClient<IDatabetGamesApiClient, DatabetGamesApiClient>(options => { options.BaseAddress = new Uri($"{gamesApiUrl}dafabet/"); })
-    .Services
-    .AddStackExchangeRedisCache(r => { r.Configuration = builderConfiguration.GetConnectionString("RedisCache"); });
+using Serilog.Sinks.Elasticsearch;
 
 
-var app = builder.Build();
-
-app.UseExceptionHandler(exceptionAppBuilder => { exceptionAppBuilder.UseMiddleware<ExceptionHandlerMiddleware>(); });
-
-if (!app.Environment.IsProduction())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    Log.Logger = new LoggerConfiguration()
+        .WriteTo.Elasticsearch(
+            nodeUris: "http://10.0.3.46:9200;",
+            indexFormat: "platipus-wallet",
+            connectionGlobalHeaders: "Authorization=Basic cGxhdGlwdXNfZWxhc3RpYzpUaGFpcmFoUGgydXNob28=",
+            autoRegisterTemplateVersion: AutoRegisterTemplateVersion.ESv7,
+            batchAction: ElasticOpType.Create,
+            typeName: null)
+        .CreateLogger();
+    
+    Log.Warning("Starting version {VersionId}", 1.0);
+    
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog(
+        (context, configuration) =>
+        {
+            configuration.EnableSelfLog(context)
+                .ReadFrom.Configuration(context.Configuration);
+        });
+
+    var builderConfiguration = builder.Configuration;
+    var services = builder.Services;
+
+    const string gamesApiUrl = "https://test.platipusgaming.com/"; //TODO to config
+    services
+        .AddTransient<ExceptionHandlerMiddleware>()
+        .AddControllers(
+            options =>
+            {
+                options.Filters.Add<SaveRequestActionFilterAttribute>(1);
+
+                options.Filters.Add<ActionResultFilterAttribute>(1);
+                options.Filters.Add<LoggingResultFilterAttribute>(2);
+            })
+        .AddJsonOptions(
+            options =>
+            {
+                options.JsonSerializerOptions.NumberHandling = JsonNumberHandling.WriteAsString | JsonNumberHandling.AllowReadingFromString;
+                options.JsonSerializerOptions.PropertyNamingPolicy = new JsonSnakeCaseNamingPolicy();
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                options.JsonSerializerOptions.Converters.Add(new JsonBoolAsNumberStringConverter());
+            })
+        .AddJsonOptions(
+            nameof(CasinoProvider.Dafabet),
+            options => { options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()); })
+        .AddJsonOptions(
+            nameof(CasinoProvider.Openbox),
+            options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                options.JsonSerializerOptions.Converters.Add(new JsonUnixDateTimeConverter());
+            })
+        .Services
+        .Configure<SupportedCurrenciesOptions>(builderConfiguration.GetSection(nameof(SupportedCurrenciesOptions)).Bind)
+        .Configure<SupportedCountriesOptions>(builderConfiguration.GetSection(nameof(SupportedCountriesOptions)).Bind)
+        .AddEndpointsApiExplorer()
+        .AddSwaggerGen()
+        .AddMediatR(Assembly.GetExecutingAssembly())
+        .AddValidatorsFromAssembly(Assembly.GetExecutingAssembly())
+        .AddAllBehaviors()
+        .AddLocalization()
+        .AddLazyCache()
+        .AddDbContext<WalletDbContext>(
+            (provider, optionsBuilder) =>
+            {
+                optionsBuilder
+                    .UseNpgsql(builderConfiguration.GetConnectionString(nameof(WalletDbContext)));
+
+                if (builder.Environment.IsDevelopment())
+                {
+                    optionsBuilder.UseLoggerFactory(provider.GetRequiredService<ILoggerFactory>());
+                    optionsBuilder.EnableSensitiveDataLogging();
+                }
+            })
+        .AddSingleton<IGamesApiClient, GamesApiClient>()
+        .AddTransient<RequestSignatureRelegatingHandler>()
+        .AddHttpClient<IGamesApiClient, GamesApiClient>(options => { options.BaseAddress = new Uri($"{gamesApiUrl}psw/"); })
+        .AddHttpMessageHandler<RequestSignatureRelegatingHandler>()
+        .Services
+        .AddSingleton<IDatabetGamesApiClient, DatabetGamesApiClient>()
+        .AddHttpClient<IDatabetGamesApiClient, DatabetGamesApiClient>(options => { options.BaseAddress = new Uri($"{gamesApiUrl}dafabet/"); })
+        .Services
+        .AddStackExchangeRedisCache(r => { r.Configuration = builderConfiguration.GetConnectionString("RedisCache"); });
+
+
+    var app = builder.Build();
+
+    app.UseExceptionHandler(exceptionAppBuilder => { exceptionAppBuilder.UseMiddleware<ExceptionHandlerMiddleware>(); });
+
+    if (!app.Environment.IsProduction())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.EnableBufferingAndSaveRawRequest();
+
+    app.UseRequestLocalization();
+
+    app.MapControllers();
+
+    app.Seed();
+
+    await app.RunAsync();
 }
 
-app.EnableBufferingAndSaveRawRequest();
-
-app.UseRequestLocalization();
-
-app.MapControllers();
-
-app.Seed();
-
-await app.RunAsync();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "FAILED!!!! on startup");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
