454ec2fc74adf533b4ec48c34a36e972029ece84
nazarkokhan <nazarkokhansomejob@gmail.com>
2022-11-10 14:52:45 +0200

    to long

--- Platipus.Wallet.Api/Application/Requests/Wallets/Openbox/Base/Response/PswBalanceResponse.cs
+++ Platipus.Wallet.Api/Application/Requests/Wallets/Openbox/Base/Response/PswBalanceResponse.cs
@@ -1,5 +1,5 @@
 namespace Platipus.Wallet.Api.Application.Requests.Wallets.Openbox.Base.Response;
 
-public record OpenboxBalanceResponse(decimal Balance);
+public record OpenboxBalanceResponse(long Balance);
 
 public record OpenboxTokenResponse(Guid Token);
