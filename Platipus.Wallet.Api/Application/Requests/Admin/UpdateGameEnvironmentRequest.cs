dce7f5eba9b9de5d56685287850753ba9f72492a
nazarkokhan <nazarkokhan@gmail.com>
2023-03-03 11:11:48 +0200

    add env update

---
+++ Platipus.Wallet.Api/Application/Requests/Admin/UpdateGameEnvironmentRequest.cs
@@ -0,0 +1,43 @@
+namespace Platipus.Wallet.Api.Application.Requests.Admin;
+
+using System.ComponentModel;
+using Domain.Entities;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+public record UpdateGameEnvironmentRequest(
+        [property: DefaultValue("test")] string Id,
+        [property: DefaultValue("https://test.platipusgaming.com/")] Uri BaseUrl,
+        [property: DefaultValue("https://platipusgaming.cloud/qa/integration/vivo/test/index.html")] Uri UisBaseUrl)
+    : IRequest<IResult>
+{
+    public class Handler : IRequestHandler<CreateGameEnvironmentRequest, IResult>
+    {
+        private readonly WalletDbContext _context;
+
+        public Handler(WalletDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IResult> Handle(
+            CreateGameEnvironmentRequest request,
+            CancellationToken cancellationToken)
+        {
+            var environment = await _context.Set<GameEnvironment>()
+                .Where(e => e.Id == request.Id)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (environment is null)
+                return ResultFactory.Failure(ErrorCode.EnvironmentAlreadyExists);
+
+            environment.BaseUrl = request.BaseUrl;
+            environment.UisBaseUrl = request.UisBaseUrl;
+
+            _context.Update(environment);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return ResultFactory.Success();
+        }
+    }
+}
