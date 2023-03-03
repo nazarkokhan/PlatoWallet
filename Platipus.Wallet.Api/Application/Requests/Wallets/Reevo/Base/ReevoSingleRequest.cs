619c1994a9a6d28157abef7b939ec0230bec445f
nazarkokhan <nazarkokhan@gmail.com>
2023-03-03 11:39:46 +0200

    fix ReevoSingleRequest.cs

--- Platipus.Wallet.Api/Application/Requests/Wallets/Reevo/Base/ReevoSingleRequest.cs
+++ Platipus.Wallet.Api/Application/Requests/Wallets/Reevo/Base/ReevoSingleRequest.cs
@@ -4,26 +4,86 @@ using JetBrains.Annotations;
 using Microsoft.AspNetCore.Mvc;
 
 [UsedImplicitly(ImplicitUseTargetFlags.WithMembers)]
-public record ReevoSingleRequest(
-    [property: FromQuery(Name = "callerId")] string CallerId,
-    [property: FromQuery(Name = "callerPassword")] string CallerPassword,
-    [property: FromQuery(Name = "action")] string Action,
-    [property: FromQuery(Name = "remote_id")] int? RemoteId,
-    [property: FromQuery(Name = "username")] string Username,
-    [property: FromQuery(Name = "game_id_hash")] string? GameIdHash,
-    [property: FromQuery(Name = "session_id")] string SessionId,
-    [property: FromQuery(Name = "gamesession_id")] string GameSessionId,
-    [property: FromQuery(Name = "key")] string Key,
-    [property: FromQuery(Name = "amount")] double? Amount,
-    [property: FromQuery(Name = "transaction_id")] string? TransactionId,
-    [property: FromQuery(Name = "round_id")] string? RoundId,
-    [property: FromQuery(Name = "gameplay_final")] int GameplayFinal,
-    [property: FromQuery(Name = "is_freeround_bet")] int? IsFreeRoundBet,
-    [property: FromQuery(Name = "freeround_id")] string? FreeRoundId,
-    [property: FromQuery(Name = "fee")] double? Fee,
-    [property: FromQuery(Name = "jackpot_contribution_in_amount")] double? JackpotContributionInAmount,
-    [property: FromQuery(Name = "is_freeround_win")] int? IsFreeRoundWin,
-    [property: FromQuery(Name = "freeround_spins_remaining")] int? FreeroundSpinsRemaining,
-    [property: FromQuery(Name = "freeround_completed")] int? FreeroundCompleted,
-    [property: FromQuery(Name = "is_jackpot_win")] int? IsJackpotWin,
-    [property: FromQuery(Name = "jackpot_win_in_amount")] double? JackpotWinInAmount) : IReevoRequest;
+public record ReevoSingleRequest : IReevoRequest
+{
+    [FromQuery(Name = "callerId")]
+    public string CallerId { get; init; } = null!;
+
+    [FromQuery(Name = "callerPassword")]
+    public string CallerPassword { get; init; } = null!;
+
+    [FromQuery(Name = "action")]
+    public string Action { get; init; } = null!;
+
+    // required for balance
+    [FromQuery(Name = "remote_id")]
+    public int? RemoteId { get; init; }
+
+    [FromQuery(Name = "username")]
+    public string Username { get; init; } = null!;
+
+    [FromQuery(Name = "game_id_hash")]
+    public string? GameIdHash { get; init; }
+
+    [FromQuery(Name = "session_id")]
+    public string SessionId { get; init; } = null!;
+
+    [FromQuery(Name = "gamesession_id")]
+    public string GameSessionId { get; init; } = null!;
+
+    [FromQuery(Name = "key")]
+    public string Key { get; init; } = null!;
+
+    //bet, win, cancel
+    [FromQuery(Name = "amount")]
+    public double? Amount { get; init; }
+
+    //bet, win, cancel
+    [FromQuery(Name = "transaction_id")]
+    public string? TransactionId { get; init; }
+
+    //bet, win, cancel
+    [FromQuery(Name = "round_id")]
+    public string? RoundId { get; init; }
+
+    //bet, win, cancel
+    [FromQuery(Name = "gameplay_final")]
+    public int GameplayFinal { get; init; }
+
+    //bet
+    [FromQuery(Name = "is_freeround_bet")]
+    public int? IsFreeRoundBet { get; init; }
+
+
+    //bet, win
+    [FromQuery(Name = "freeround_id")]
+    public string? FreeRoundId { get; init; }
+
+    //bet, win
+    [FromQuery(Name = "fee")]
+    public double? Fee { get; init; }
+
+    //bet, win
+    [FromQuery(Name = "jackpot_contribution_in_amount")]
+    public double? JackpotContributionInAmount { get; init; }
+
+    //win
+    [FromQuery(Name = "is_freeround_win")]
+    public int? IsFreeRoundWin { get; init; }
+
+    //win
+    [FromQuery(Name = "freeround_spins_remaining")]
+    public int? FreeroundSpinsRemaining { get; init; }
+
+    //win
+    [FromQuery(Name = "freeround_completed")]
+    public int? FreeroundCompleted { get; init; }
+
+    //win
+    [FromQuery(Name = "is_jackpot_win")]
+    public int? IsJackpotWin { get; init; }
+
+    //win
+    [FromQuery(Name = "jackpot_win_in_amount")]
+    public double? JackpotWinInAmount { get; init; }
+}
