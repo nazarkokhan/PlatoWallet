0df4355edd44bf0aa05d10b08286020b9e8cbf5e
nazarkokhan <nazarkokhan@gmail.com>
2023-06-08 15:00:19 +0300

    fix missing files

--- Platipus.Wallet.Domain/Entities/Abstract/Generic/Entity.cs
+++ Platipus.Wallet.Domain/Entities/Abstract/Generic/Entity.cs
@@ -1,6 +1,8 @@
 namespace Platipus.Wallet.Domain.Entities.Abstract.Generic;
 
-public abstract class Entity<T> : AuditableEntity
+public abstract class Entity<T>
 {
     public T Id { get; set; } = default!;
+    public DateTime CreatedDate { get; set; } = default!;
+    public DateTime LastUpdatedDate { get; set; } = default!;
 }
---
+++ Platipus.Wallet.Infrastructure/Persistence/NpgsqlFunctions.cs
@@ -0,0 +1,6 @@
+namespace Platipus.Wallet.Infrastructure.Persistence;
+
+public static class NpgsqlFunctions
+{
+    public static DateTime CurrentTimestamp() => throw new NotImplementedException();
+}
