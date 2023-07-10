55de0f966777e6bb39897ce9cad3f764c7548b9c
d.stepanenko <d.stepanenko@platipusgaming.com>
2023-07-10 12:16:53 +0300

    Update AtlasWinRequest.cs

--- Platipus.Wallet.Api/Application/Requests/Wallets/Atlas/AtlasWinRequest.cs
+++ Platipus.Wallet.Api/Application/Requests/Wallets/Atlas/AtlasWinRequest.cs
@@ -66,8 +66,8 @@ public sealed record AtlasWinRequest(
                 .WithMessage("RoundId cannot exceed 150 characters.");
 
             RuleFor(x => x.Amount)
-                .GreaterThan(0)
-                .WithMessage("Amount must be greater than 0.");
+                .NotEmpty()
+                .WithMessage("Amount property is required");
 
             RuleFor(x => x.TransactionId)
                 .NotEmpty()
