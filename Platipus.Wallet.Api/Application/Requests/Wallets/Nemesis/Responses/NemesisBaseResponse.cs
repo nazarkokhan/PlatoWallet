bc93dc7ec81845c39a4d6ec075e776d5973ea183
d.stepanenko <d.stepanenko@platipusgaming.com>
2023-09-08 10:36:08 +0300

    Update NemesisBaseResponse.cs

--- Platipus.Wallet.Api/Application/Requests/Wallets/Nemesis/Responses/NemesisBaseResponse.cs
+++ Platipus.Wallet.Api/Application/Requests/Wallets/Nemesis/Responses/NemesisBaseResponse.cs
@@ -1,11 +1,13 @@
 namespace Platipus.Wallet.Api.Application.Requests.Wallets.Nemesis.Responses;
 
+using System.Text.Json.Serialization;
 using JetBrains.Annotations;
 
 [PublicAPI]
 public record NemesisBaseResponse
 {
+    [JsonIgnore]
     public long Decimals { get; init; } = 2;
-    public bool Duplicated { get; init; } = false;
+    public bool Duplicated { get; init; }
     public long Timestamp { get; init; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 }
