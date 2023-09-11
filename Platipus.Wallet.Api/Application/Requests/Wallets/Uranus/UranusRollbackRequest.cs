198fb1fc388cece3471ca5c6e16169c6565180c5
d.stepanenko <d.stepanenko@platipusgaming.com>
2023-09-11 09:35:58 +0300

    Update UranusRollbackRequest.cs

--- Platipus.Wallet.Api/Application/Requests/Wallets/Uranus/UranusRollbackRequest.cs
+++ Platipus.Wallet.Api/Application/Requests/Wallets/Uranus/UranusRollbackRequest.cs
@@ -9,85 +9,87 @@ using Platipus.Wallet.Api.Application.Results.Uranus.WithData;
 using Platipus.Wallet.Api.Application.Services.Wallet;
 
 public sealed record UranusRollbackRequest(
-    string SessionToken, 
-    string PlayerId, 
+    string SessionToken,
+    string PlayerId,
     string GameId,
     string RollbackTransactionId,
     string RoundId,
-    string TransactionId, 
-    string Amount, 
-    string Currency, 
+    string TransactionId,
+    string Amount,
+    string Currency,
     string? Payload) : IUranusRequest, IRequest<IUranusResult<UranusSuccessResponse<UranusCommonDataWithTransaction>>>
 {
-    public sealed class Handler 
+    public sealed class Handler
         : IRequestHandler<UranusRollbackRequest, IUranusResult<UranusSuccessResponse<UranusCommonDataWithTransaction>>>
     {
         private readonly IWalletService _walletService;
 
-        public Handler(IWalletService walletService) => 
+        public Handler(IWalletService walletService) =>
             _walletService = walletService;
 
         public async Task<IUranusResult<UranusSuccessResponse<UranusCommonDataWithTransaction>>> Handle(
-            UranusRollbackRequest request, 
+            UranusRollbackRequest request,
             CancellationToken cancellationToken)
         {
             var walletResult = await _walletService.RollbackAsync(
                 request.SessionToken,
                 request.RoundId,
                 request.RollbackTransactionId,
-                amount: (int)double.Parse(request.Amount),
+                amount: decimal.Parse(request.Amount),
                 cancellationToken: cancellationToken);
 
-            if (walletResult.IsFailure || walletResult.Data is null)
+            if (walletResult.IsFailure)
                 return walletResult.ToUranusFailureResult<UranusSuccessResponse<UranusCommonDataWithTransaction>>();
+
+            var walletData = walletResult.Data;
             var response = new UranusSuccessResponse<UranusCommonDataWithTransaction>(
-             new UranusCommonDataWithTransaction(
-                 walletResult.Data?.Currency, 
-                 walletResult.Data!.Balance,
-                 walletResult.Data.Transaction.Id
-                 ));
+                new UranusCommonDataWithTransaction(
+                    walletData.Currency,
+                    walletData.Balance,
+                    walletData.Transaction.Id));
+
             return UranusResultFactory.Success(response);
         }
     }
-    
+
     public sealed class EvoplayRollbackRequestValidator : AbstractValidator<UranusRollbackRequest>
     {
         public EvoplayRollbackRequestValidator()
         {
             RuleFor(x => x.SessionToken)
-                .NotEmpty()
-                .Length(1, 255);
+               .NotEmpty()
+               .Length(1, 255);
 
             RuleFor(x => x.PlayerId)
-                .NotEmpty()
-                .Length(1, 255);
+               .NotEmpty()
+               .Length(1, 255);
 
             RuleFor(x => x.GameId)
-                .NotEmpty()
-                .Length(1, 255);
+               .NotEmpty()
+               .Length(1, 255);
 
             RuleFor(x => x.TransactionId)
-                .NotEmpty()
-                .Length(1, 255);
+               .NotEmpty()
+               .Length(1, 255);
 
             RuleFor(x => x.RollbackTransactionId)
-                .NotEmpty()
-                .Length(1, 255);
+               .NotEmpty()
+               .Length(1, 255);
 
             RuleFor(x => x.Amount)
-                .Cascade(CascadeMode.Stop)
-                .NotEmpty()
-                .MinimumLength(1)
-                .Matches(@"^\d+(\.\d{1,10})?$")
-                .WithMessage("The amount should be a numeric string with up to 10 decimal points.");
+               .Cascade(CascadeMode.Stop)
+               .NotEmpty()
+               .MinimumLength(1)
+               .Matches(@"^\d+(\.\d{1,10})?$")
+               .WithMessage("The amount should be a numeric string with up to 10 decimal points.");
 
             RuleFor(x => x.Currency)
-                .NotEmpty()
-                .Length(3, 4);
+               .NotEmpty()
+               .Length(3, 4);
 
             RuleFor(x => x.RoundId)
-                .NotEmpty()
-                .MaximumLength(255);
+               .NotEmpty()
+               .MaximumLength(255);
         }
     }
 }
