70676030b22ac7bc39b8ef5682be9c0b5d5e926f
d.stepanenko <d.stepanenko@platipusgaming.com>
2023-09-14 15:18:37 +0300

    Update WalletProvider.cs
    removing redundant GamesGlobal from WalletProvider, making enum values have their own specific order.

--- Platipus.Wallet.Domain/Entities/Enums/WalletProvider.cs
+++ Platipus.Wallet.Domain/Entities/Enums/WalletProvider.cs
@@ -1,26 +1,25 @@
 namespace Platipus.Wallet.Domain.Entities.Enums;
 
-public enum WalletProvider
+public enum WalletProvider : short
 {
-    Psw,
-    Dafabet,
-    Openbox,
-    Hub88,
-    Softswiss,
-    Sw,
-    SoftBet,
-    GamesGlobal,
-    Uis,
-    Betflag,
-    Reevo,
-    Everymatrix,
-    BetConstruct,
-    Parimatch,
-    EmaraPlay,
-    Atlas,
-    Uranus,
-    Evenbet,
-    Anakatech,
-    Nemesis,
-    Synot
+    Psw = 0,
+    Dafabet = 1,
+    Openbox = 2,
+    Hub88 = 3,
+    Softswiss = 4,
+    Sw = 5,
+    SoftBet = 6,
+    Uis = 8,
+    Betflag = 9,
+    Reevo = 10,
+    Everymatrix = 11,
+    BetConstruct = 12,
+    Parimatch = 13,
+    EmaraPlay = 14,
+    Atlas = 15,
+    Uranus = 16,
+    Evenbet = 17,
+    Anakatech = 18,
+    Nemesis = 19,
+    Synot = 20
 }
