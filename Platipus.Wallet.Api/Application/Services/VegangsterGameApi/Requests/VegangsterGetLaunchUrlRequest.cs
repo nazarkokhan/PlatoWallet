cc0cdf0469e538f138019a3687c65143553ea2a2
d.stepanenko <d.stepanenko@platipusgaming.com>
2023-09-15 17:57:42 +0300

    developing ExternalVegangsterController, etc.

---
+++ Platipus.Wallet.Api/Application/Services/VegangsterGameApi/Requests/VegangsterGetLaunchUrlRequest.cs
@@ -0,0 +1,62 @@
+﻿namespace Platipus.Wallet.Api.Application.Services.VegangsterGameApi.Requests;
+
+using Domain.Entities;
+using External;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Results.ResultToResultMappers;
+using Results.Vegangster;
+using Results.Vegangster.WithData;
+using Wallet;
+
+public sealed record VegangsterGetLaunchUrlRequest(
+    string Environment,
+    string CasinoId,
+    VegangsterGetLaunchUrlGameApiRequest ApiRequest) : IRequest<IVegangsterResult<VegangsterGetLaunchUrlResponse>>
+{
+    public sealed class
+        Handler : IRequestHandler<VegangsterGetLaunchUrlRequest, IVegangsterResult<VegangsterGetLaunchUrlResponse>>
+    {
+        private readonly IWalletService _walletService;
+        private readonly WalletDbContext _walletDbContext;
+        private readonly IVegangsterGameApiClient _vegangsterGameApiClient;
+
+        public Handler(
+            IWalletService walletService,
+            IVegangsterGameApiClient vegangsterGameApiClient,
+            WalletDbContext walletDbContext)
+        {
+            _walletService = walletService;
+            _vegangsterGameApiClient = vegangsterGameApiClient;
+            _walletDbContext = walletDbContext;
+        }
+
+        public async Task<IVegangsterResult<VegangsterGetLaunchUrlResponse>> Handle(
+            VegangsterGetLaunchUrlRequest request,
+            CancellationToken cancellationToken)
+        {
+            var walletResponse = await _walletService.GetEnvironmentAsync(
+                request.Environment,
+                cancellationToken);
+
+            var casinoXApiSignature = await _walletDbContext.Set<Casino>()
+               .Where(c => c.Id == request.CasinoId)
+               .Select(x => x.Params.VegangsterPrivateWalletSecuritySign)
+               .FirstOrDefaultAsync(cancellationToken: cancellationToken);
+
+            var clientResponse = await _vegangsterGameApiClient.GetLaunchUrlAsync(
+                walletResponse.Data.BaseUrl,
+                request.CasinoId,
+                casinoXApiSignature!,
+                request.ApiRequest,
+                cancellationToken);
+
+            if (clientResponse.IsFailure)
+                return clientResponse.ToVegangsterFailureResult<VegangsterGetLaunchUrlResponse>();
+
+            var response = new VegangsterGetLaunchUrlResponse(clientResponse.Data.Data.Url);
+
+            return VegangsterResultFactory.Success(response);
+        }
+    }
+}
