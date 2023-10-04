b1394e2ab2e5bf8d7194d53002471a6eac24873c
d.stepanenko <d.stepanenko@platipusgaming.com>
2023-10-04 13:13:02 +0300

    Update EverymatrixGetLaunchUrlGameApiRequest.cs

--- Platipus.Wallet.Api/Application/Services/EverymatrixGameApi/External/EverymatrixGetLaunchUrlGameApiRequest.cs
+++ Platipus.Wallet.Api/Application/Services/EverymatrixGameApi/External/EverymatrixGetLaunchUrlGameApiRequest.cs
@@ -5,11 +5,11 @@ using Microsoft.AspNetCore.Mvc;
 
 public sealed record EverymatrixGetLaunchUrlGameApiRequest(
     [property: BindProperty(Name = "brand")] string Brand,
-    [property: JsonPropertyName("gameCode")] [property: BindProperty(Name = "gamecode")] string GameCode,
+    [property: JsonPropertyName("gameCode")] [property: BindProperty(Name = "gameCode")] string GameCode,
     [property: BindProperty(Name = "language")] string Language,
-    [property: JsonPropertyName("freePlay")] [property: BindProperty(Name = "freeplay")] bool FreePlay,
+    [property: JsonPropertyName("freePlay")] [property: BindProperty(Name = "freePlay")] bool FreePlay,
     [property: BindProperty(Name = "mobile")] bool Mobile,
     [property: BindProperty(Name = "mode")] string Mode,
     [property: BindProperty(Name = "token")] string Token,
-    [property: JsonPropertyName("lobbyUrl")] [property: BindProperty(Name = "lobbyurl")] string LobbyUrl,
-    [property: JsonPropertyName("currencyCode")] [property: BindProperty(Name = "currencycode")] string CurrencyCode);
+    [property: JsonPropertyName("lobbyUrl")] [property: BindProperty(Name = "lobbyUrl")] string LobbyUrl,
+    [property: JsonPropertyName("currencyCode")] [property: BindProperty(Name = "currencyCode")] string CurrencyCode);
